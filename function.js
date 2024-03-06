3f8970da9a04807df6c4c49182ed335181e87d45
SaliouGlide <162391243+SaliouGlide@users.noreply.github.com>
2024-03-06 23:08:19 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,32 +1,40 @@
-window.function = async function(src, ctnt) {
+window.function = async function(src, ctnt, CheckRep) {
   src = src.value ?? "";
   ctnt = ctnt.value ?? "";
   
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://api.chatpdf.com/v1/chats/message');
-    xhr.setRequestHeader('x-api-key', 'sec_oVR5DxgcOAjbB4eFwiObjHtzH1U9IpiB');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = function () {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        resolve(xhr.responseText);
-      } else {
-        // Gérez les erreurs côté serveur et renvoyez-les comme message d'erreur
-        const errorMessage = xhr.statusText || 'Erreur lors de la requête';
-        reject(new Error(errorMessage));
-      }
-    };
-    xhr.onerror = function () {
-      // Affichez les erreurs réseau
-      reject(new Error('Erreur réseau lors de la requête'));
-    };
-    const data = JSON.stringify({
-      sourceId: src,
-      messages: [{
-        role: 'user',
-        content: ctnt
-      }]
+  // Vérifier si la colonne Réponse est vide
+  if (CheckRep === "") {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', 'https://api.chatpdf.com/v1/chats/message');
+      xhr.setRequestHeader('x-api-key', 'sec_oVR5DxgcOAjbB4eFwiObjHtzH1U9IpiB');
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+          resolve(xhr.responseText);
+        } else {
+          // Gérez les erreurs côté serveur et renvoyez-les comme message d'erreur
+          const errorMessage = xhr.statusText || 'Erreur lors de la requête';
+          reject(new Error(errorMessage));
+        }
+      };
+      xhr.onerror = function () {
+        // Affichez les erreurs réseau
+        reject(new Error('Erreur réseau lors de la requête'));
+      };
+      const data = JSON.stringify({
+        sourceId: src,
+        messages: [{
+          role: 'user',
+          content: ctnt
+        }]
+      });
+      xhr.send(data);
     });
-    xhr.send(data);
-  });
+  } else {
+    // Colonne réponse non vide, renvoyer une réponse déjà envoyée
+    return new Promise((resolve, reject) => {
+      reject(new Error('Réponse déjà envoyée'));
+    });
+  }
 };
