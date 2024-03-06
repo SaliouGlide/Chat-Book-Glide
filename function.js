89d9ed342aebdc5046369d83c2072de08f7c7f04
SaliouGlide <162391243+SaliouGlide@users.noreply.github.com>
2024-03-06 21:19:26 +0100

    Update function.js

--- function.js
+++ function.js
@@ -6,7 +6,6 @@ window.function = async function(src, ctnt) {
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://api.chatpdf.com/v1/chats/message');
     xhr.setRequestHeader('x-api-key', 'sec_oVR5DxgcOAjbB4eFwiObjHtzH1U9IpiB');
-    xhr.setRequestHeader('Accept', 'application/json');
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onload = function () {
       if (xhr.status >= 200 && xhr.status < 300) {
@@ -31,4 +30,3 @@ window.function = async function(src, ctnt) {
     xhr.send(data);
   });
 };
-return xhr.status;
